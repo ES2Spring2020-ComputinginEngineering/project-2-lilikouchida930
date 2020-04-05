91cd382c28a3948b5e6871ac2c3e6c99f4b59035
lilikouchida930 <Ririko.Uchida@tufts.edu>
2020-04-05 12:30:29 -0400

    Update KMeansClustering_functions.py

--- KMeansClustering_functions.py
+++ KMeansClustering_functions.py
@@ -5,6 +5,23 @@ import matplotlib.pyplot as plt
 import random
 
 
+#FUNCTIONS
+
 def openckdfile():
     glucose, hemoglobin, classification = np.loadtxt('ckd.csv', delimiter=',', skiprows=1, unpack=True)
     return glucose, hemoglobin, classification
+
+#...........
+    
+def normalizeData(glucose, hemoglobin, classification):
+#Normalizes each of the three data sets to a unitless scale of 0 to 1. 
+    glucoseNorm = (glucose-np.amin(glucose))/(np.amax(glucose)-np.amin(glucose))
+    hemoglobinNorm = (hemoglobin-np.amin(hemoglobin))/(np.amax(hemoglobin)-np.amin(hemoglobin))
+    classNorm = (classification-np.amin(classification))/(np.amax(classification)-np.amin(classification))
+    return glucoseNorm, hemoglobinNorm, classNorm
+
+#...........
+
+#MAIN SCRIPT
+glucose, hemoglobin, classification = openckdfile()
+glucoseNorm, hemoglobinNorm, classNorm = normalizeData(glucose, hemoglobin, classification)   
