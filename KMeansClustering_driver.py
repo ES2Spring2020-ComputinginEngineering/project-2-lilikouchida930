b6c9321c249962e42983ef40fef4a14dd3d97b97
lilikouchida930 <Ririko.Uchida@tufts.edu>
2020-04-11 19:48:38 -0400

    Update KMeansClustering_driver.py

--- KMeansClustering_driver.py
+++ KMeansClustering_driver.py
@@ -1,2 +1,14 @@
 #Please place your FUNCTION code for step 4 here.
 import KMeansClustering_functions as kmc #Use kmc to call your functions
+
+
+#MAIN SCRIPT
+glucose, hemoglobin, classification = kmc.openckdfile()
+glucoseNorm, hemoglobinNorm, classNorm = kmc.normalizeData(glucose, hemoglobin, classification)   
+initialClusterArray_x, initialClusterArray_y = kmc.initializeClusters(2)
+distances = kmc.findDistance(hemoglobinNorm, initialClusterArray_x, initialClusterArray_y)
+nearestCentroid = kmc.findNearestCentroid()
+updatedArray_x, updatedArray_y = kmc.updateCentroids(2)
+        
+updatedArray_x, updatedArray_y, nearestCentroid = kmc.untilNoChange(2, updatedArray_x, updatedArray_y, initialClusterArray_x, initialClusterArray_y)
+estimatedClasses = kmc.estimatedClass(2)
