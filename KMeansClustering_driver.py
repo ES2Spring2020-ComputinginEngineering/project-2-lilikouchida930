9539b915fc1ffde429713fbc78b997bc64ca76aa
lilikouchida930 <Ririko.Uchida@tufts.edu>
2020-04-12 15:30:09 -0400

    Update KMeansClustering_driver.py

--- KMeansClustering_driver.py
+++ KMeansClustering_driver.py
@@ -3,12 +3,17 @@ import KMeansClustering_functions as kmc #Use kmc to call your functions
 
 
 #MAIN SCRIPT
+
 glucose, hemoglobin, classification = kmc.openckdfile()
-glucoseNorm, hemoglobinNorm, classNorm = kmc.normalizeData(glucose, hemoglobin, classification)   
+glucoseNorm, hemoglobinNorm, classNorm = kmc.normalizeData(glucose, hemoglobin, classification)
 initialClusterArray_x, initialClusterArray_y = kmc.initializeClusters(2)
 distances = kmc.findDistance(hemoglobinNorm, initialClusterArray_x, initialClusterArray_y)
 nearestCentroid = kmc.findNearestCentroid()
-updatedArray_x, updatedArray_y = kmc.updateCentroids(2)
-        
-updatedArray_x, updatedArray_y, nearestCentroid = kmc.untilNoChange(2, updatedArray_x, updatedArray_y, initialClusterArray_x, initialClusterArray_y)
-estimatedClasses = kmc.estimatedClass(2)
+updatedArray_x = kmc.np.zeros(2)
+updatedArray_y = kmc.np.zeros(2)
+updatedArray_x,updatedArray_y = kmc.updateCentroids(2)
+updatedArray_x, updatedArray_y = kmc.untilNoChange(2, updatedArray_x, updatedArray_y,initialClusterArray_x, initialClusterArray_y)
+distancesFinal = kmc.findDistanceFinal(hemoglobinNorm, updatedArray_x, updatedArray_y)
+nearestCentroidFinal = kmc.findNearestCentroidFinal()
+corr_lab_CKD, corr_lab_nonCKD = kmc.calculatePercentages()
+kmc.graphingKMeans(glucoseNorm, hemoglobinNorm, classNorm, updatedArray_x, updatedArray_y)
